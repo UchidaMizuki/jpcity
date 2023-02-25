9216267f17c400af1eb9ac17015c9aad1e5ded13
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-02-26 01:18:38 +0900

    Add `city_desig_code`

---
+++ data-raw/city_desig_code.R
@@ -0,0 +1,27 @@
+source("data-raw/setup.R")
+
+# city_desig_code ---------------------------------------------------------
+
+city_code <- graph_city |>
+  activate(nodes) |>
+  as_tibble() |>
+  filter(!is.na(city_code)) |>
+  select(!interval) |>
+  mutate(city_desig_name = if_else(str_starts(city_code, "131"),
+                                   "特別区部",
+                                   city_desig_name),
+         city_desig_name_kana = if_else(str_starts(city_code, "131"),
+                                        "とくべつくぶ",
+                                        city_desig_name_kana))
+
+city_desig_code <- city_code |>
+  filter(city_code == "13100" | !is.na(city_desig_name) & is.na(city_name)) |>
+  select(!c(city_name, city_name_kana))
+
+city_desig_code <- city_code |>
+  inner_join(city_desig_code |>
+              rename(city_desig_code = city_code),
+            by = join_by(pref_name, city_desig_name, city_desig_name_kana)) |>
+  select(city_code, city_desig_code)
+
+write_rds(city_desig_code, "data-raw/city_desig_code.rds")
--- data-raw/internal.R
+++ data-raw/internal.R
@@ -6,6 +6,9 @@ graph_city <- read_rds("data-raw/graph_city.rds")
 interval_graph_city <- graph_city$interval
 graph_city <- graph_city$graph
 
+city_desig_code <- read_rds("data-raw/city_desig_code.rds")
+
 usethis::use_data(graph_city, interval_graph_city,
+                  city_desig_code,
                   internal = TRUE,
                   overwrite = TRUE)
